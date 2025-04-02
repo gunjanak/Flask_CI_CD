a54cdc502a4b2043887aed67469ed49bba0c2feb
gunjanak <janak.klal@gmail.com>
2025-04-02 18:10:51 +0545

    html added

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello and "
+    return render_template('index.html', dynamic_text="Hello from Flask!")
 
 if __name__ == '__main__':
     app.run()
