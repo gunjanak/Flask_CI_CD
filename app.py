55a9c2bd6b3151e0e4269389e91659579aeb28af
gunjanak <janak.klal@gmail.com>
2025-04-02 17:54:08 +0545

    text changed again

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World again and again and again! and fuck you"
+    return "Hello"
 
 if __name__ == '__main__':
     app.run()
